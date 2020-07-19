b987972bc448fc273af93dd1f450d78412e5e83a
nitz <nitz@users.noreply.github.com>
2020-07-18 23:13:03 -0400

    Add fatal error to vscode problem matcher.

--- powerOverwhelmiing/app/app_utilities.h
+++ powerOverwhelmiing/app/app_utilities.h
@@ -3,5 +3,6 @@
 #define ALWAYS_INLINE __attribute__((always_inline))
 #define WEAK __attribute__((weak))
 #define NO_RETURN __attribute__((noreturn))
+#define NO_OPT __attribute__((optimize("O0")))
 
 void __PANIC() NO_RETURN;
